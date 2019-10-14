dfd5f81a93fe930ec8648af91cb65aa3f564c682
Alexandra Ferrari <alexandraferrari@github.com>
2019-10-14 16:05:46 -0300

    add model tabela eventos

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from core.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+  '''
+    Define os campos que serão exibidos na tela de admin
+  '''
+  list_display = ('titulo', 'data_evento', 'data_criacao')
+  list_filter = ('titulo', 'data_evento')
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -2,5 +2,19 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+  titulo = models.CharField(max_length=100)
+  descricao = models.TextField(blank=True, null=True)
+  data_evento = models.DateTimeField(verbose_name='Data do Evento')
+  data_criacao = models.DateTimeField(auto_now=True)
+  usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'evento' #define table name difente do default
+
+  def __str__(self):
+    return self.titulo #para retornar o titulo
